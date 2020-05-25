9f37b67dad23f342710f6b3c9433de76d5041858
Deven <devenbhalerao@gmail.com>
2020-05-25 16:10:17 +0530

    replaced repository with new project

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
+  PORT: process.env.PORT || 5000,
+  MONGODB_URL:
+    process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/nonvegmart',
+  JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
+  PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+};
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,35 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './_reducers';
-
-const createStoreWithMiddleware = applyMiddleware(
-	promiseMiddleware,
-	ReduxThunk
-)(createStore);
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './store';
 
 ReactDOM.render(
-	<Provider
-		store={createStoreWithMiddleware(
-			Reducer,
-			window.__REDUX_DEVTOOLS_EXTENSION__ &&
-				window.__REDUX_DEVTOOLS_EXTENSION__()
-		)}
-	>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById('root')
-);
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
+

f52b57d8efdd266bbf00571223edd205a610b93c
Deven <devenbhalerao@gmail.com>
2020-05-25 18:26:38 +0530

    fixed image upload

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ import productRoute from './routes/productRoute';
 import orderRoute from './routes/orderRoute';
 
 const mongodbUrl = config.MONGODB_URL;
-mongoose.connect(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-}).catch((error) => console.log(error.reason));
-
+mongoose
+  .connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .catch((error) => console.log(error.reason));
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,9 +26,11 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 
-app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.use(express.static(path.join(__dirname, '/../frontend/images')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+  res.sendFile(path.join(`${__dirname}/../frontend/public/index.html`));
 });
 
-app.listen(config.PORT, () => { console.log('Server started at http://localhost:5000'); });
+app.listen(config.PORT, () => {
+  console.log('Server started at http://localhost:5000');
+});

81962c8ee336f5ce6822def834596a13478b2cbe
Deven <devenbhalerao@gmail.com>
2020-06-01 20:32:26 +0530

    deploy heroku 1

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,8 @@ dotenv.config();
 export default {
   PORT: process.env.PORT || 5000,
   MONGODB_URL:
-    process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/nonvegmart',
+    process.env.MONGODB_URL ||
+    /* 'mongodb://127.0.0.1:27017/nonvegmart*/ 'mongodb+srv://deven:deven@nonvegmart-qbfql.mongodb.net/test?retryWrites=true&w=majority',
   JWT_SECRET: /*process.env.JWT_SECRET ||*/ 'somethingsecret',
 
   //PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
